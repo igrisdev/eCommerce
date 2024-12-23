bfaed6e5121cc5b4b24174f9428339f6989f8ead
igrisdev <johanalvarezz237@gmail.com>
2024-12-23 14:21:00 -0500

    add Home and Products pages, configure Tailwind CSS, and set up routing

---
+++ src/router/Router.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router'
+
+import Home from '../pages/Home'
+import Products from '../pages/Products'
+
+export const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='products' element={<Products />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
